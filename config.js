387c85fb6a6ac08dd8fe0705674ee0010c349362
blade_xo <89949214+bladexo@users.noreply.github.com>
2023-10-10 10:15:06 +0100

    Update config.js

--- config.js
+++ config.js
@@ -17,13 +17,13 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["8801975492880","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801975492880","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801975492880"];
-global.OwnerName = "Slasher";
-global.BotName = "XLICON-MD";
-global.packname = "X-2.0 Bot";                             //Do not change.
-global.author = "By: Slasher";                               //Do not change.
+global.Owner = ["2349054881711","27693016969"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ["27693016969","2349054881711"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.ownertag = ["27693016969"];
+global.OwnerName = "KATLEGO";
+global.BotName = "DINNO-MD";
+global.packname = "2349054881711";                             //Do not change.
+global.author = "By: DINNO";                               //Do not change.
 global.BotSourceCode = "https://github.com/ahil15/Xlicon-v2"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/C4ivwZKuh5bLJkqfYNPQsk"; 
 
